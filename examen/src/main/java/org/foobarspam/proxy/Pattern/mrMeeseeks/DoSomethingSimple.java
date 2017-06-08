cfa27c6ad6c02c70556e332bc3efd9a66705604e
Adrián Rojas <adriian12@users.noreply.github.com>
2017-06-08 17:05:01 +0200

    clases creadas

---
+++ examen/src/main/java/org/foobarspam/proxy/Pattern/mrMeeseeks/DoSomethingSimple.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.foobarspam.proxy.Pattern.mrMeeseeks;
+
+/**
+ * @author adrian
+ *
+ */
+public interface DoSomethingSimple {
+
+}
---
+++ examen/src/main/java/org/foobarspam/proxy/Pattern/mrMeeseeks/MrMeeseeks.java
@@ -0,0 +1,5 @@
+package org.foobarspam.proxy.Pattern.mrMeeseeks;
+
+public class MrMeeseeks implements Doable {
+
+}
